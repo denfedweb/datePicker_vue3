1d47259a4db176d3c121274e351d54a47140efe5
Denis Fedorovici <denfedweb@mail.ru>
2023-02-13 13:07:43 +0200

    Update dateModule.js

--- src/modules/dateModule.js
+++ src/modules/dateModule.js
@@ -1,16 +1,16 @@
 export default function (){
   Date.prototype.getWeek = function() {
-    let onejan = new Date(this.getFullYear(),0,1);
-    let today = new Date(this.getFullYear(),this.getMonth(),this.getDate());
-    let dayOfYear = ((today - onejan + 86400000)/86400000);
+    let onejan = new Date(this.getFullYear(),0,1)
+    let today = new Date(this.getFullYear(),this.getMonth(),this.getDate())
+    let dayOfYear = ((today - onejan + 86400000)/86400000)
     return Math.ceil(dayOfYear/7)
   }
   Date.prototype.addDays = function(days) {
     let dat = new Date(this.valueOf())
-    dat.setDate(dat.getDate() + days);
-    return dat;
+    dat.setDate(dat.getDate() + days)
+    return dat
   }
     Date.prototype.timeNow = function () {
-      return ((this.getHours() < 10)?"0":"") + this.getHours() +":"+ ((this.getMinutes() < 10)?"0":"") + this.getMinutes();
+      return ((this.getHours() < 10)?"0":"") + this.getHours() +":"+ ((this.getMinutes() < 10)?"0":"") + this.getMinutes()
   }
 }
